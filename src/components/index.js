4892988d3bd2ffaebb8cc091fd376173b07f3a9f
Andrew Cheung <cheungdzinyung@gmail.com>
2021-12-19 21:52:18 -0500

    cecntralize export

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export * from "./ActivityFeed/ActivityFeed";
+export * from "./ActivityDetail/ActivityDetail";
+export * from "./Archive/Archive";
+export * from "./Header/Header";
+export * from "./Footer/Footer";
+export * from "./Tab/Tab";
