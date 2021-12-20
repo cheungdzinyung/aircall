bc1e9a6dbed455a5d63ea14fd0e5ae0efc1937c0
Andrew Cheung <cheungdzinyung@gmail.com>
2021-12-19 21:46:11 -0500

    renamed Header fiel to keep consistent

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Header = () => {
+export const Header = () => {
   return (
     <header>
       <svg width='486px' height='168px' viewBox='0 0 486 168' version='1.1' xmlns='http://www.w3.org/2000/svg'>
@@ -21,5 +21,3 @@ const Header = () => {
     </header>
   );
 };
-
-export default Header;
