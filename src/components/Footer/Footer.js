69618e74717108cb4fe84899984bba3c19a24b82
Andrew Cheung <cheungdzinyung@gmail.com>
2021-12-19 21:50:57 -0500

    Footer component with css

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { MdDialpad, MdContactPhone, MdMessage } from "react-icons/md";
+import "../../css/Footer.css";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <MdContactPhone />
+      <MdDialpad />
+      <MdMessage />
+    </footer>
+  );
+};
